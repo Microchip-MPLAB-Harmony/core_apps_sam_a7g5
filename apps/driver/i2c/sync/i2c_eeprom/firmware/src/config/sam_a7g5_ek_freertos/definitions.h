28779a436e0097ee9f0e30c6e6a08b84de59e052
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-09-08 00:43:04 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_sync_i2c_eeprom_sam_a7g5_ek_freertos
    MH3-59575
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_a7g5_ek_freertos/definitions.h
+++ apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_a7g5_ek_freertos/definitions.h
@@ -59,11 +59,11 @@
 #include "task.h"
 #include "peripheral/flexcom/twi/master/plib_flexcom1_twi_master.h"
 #include "bsp/bsp.h"
+#include "driver/i2c/drv_i2c.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "driver/i2c/drv_i2c.h"
 #include "app_i2c_eeprom.h"
 
 
