c48f1602a0158717f753ab02866517cfa5e48169
Syam Nair <syam.nair@microchip.com>
2022-06-02 23:23:35 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_multi_slave_sam_a7g5_ek
    MH3-65080
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_a7g5_ek/bsp/bsp.h
+++ apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_a7g5_ek/bsp/bsp.h
@@ -60,14 +60,16 @@
 // Section: BSP Macros
 // *****************************************************************************
 // *****************************************************************************
-
-#define PIOA_REGS       ((pio_group_registers_t*)(&(PIO_REGS->PIO_GROUP[0])))
-#define PIOB_REGS       ((pio_group_registers_t*)(&(PIO_REGS->PIO_GROUP[1])))
-#define PIOC_REGS       ((pio_group_registers_t*)(&(PIO_REGS->PIO_GROUP[2])))
-#define PIOD_REGS       ((pio_group_registers_t*)(&(PIO_REGS->PIO_GROUP[3])))
-#define PIOE_REGS       ((pio_group_registers_t*)(&(PIO_REGS->PIO_GROUP[4])))
-
-
+/*PIOA base address */
+#define PIOA_REGS   ((pio_group_registers_t*)(&(PIO_REGS->PIO_GROUP[0])))
+/*PIOB base address */
+#define PIOB_REGS   ((pio_group_registers_t*)(&(PIO_REGS->PIO_GROUP[1])))
+/*PIOC base address */
+#define PIOC_REGS   ((pio_group_registers_t*)(&(PIO_REGS->PIO_GROUP[2])))
+/*PIOD base address */
+#define PIOD_REGS   ((pio_group_registers_t*)(&(PIO_REGS->PIO_GROUP[3])))
+/*PIOE base address */
+#define PIOE_REGS   ((pio_group_registers_t*)(&(PIO_REGS->PIO_GROUP[4])))
 
 /*** LED Macros for LED_RED ***/
 #define LED_RED_Toggle() do { PIOB_REGS->PIO_MSKR = (1U<<8); (PIOB_REGS->PIO_ODSR ^= (1U<<8)); } while (0)
